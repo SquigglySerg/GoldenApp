93dfbf62dee9feec92b4806c73b78836f52100f0
Sergio Rodriguez <Sergio Rodriguez>
2017-02-27 16:15:10 -0700

    Changed some of the map's configuration and removed some unused imports in the newfeed

--- app/src/main/java/twistedgiraffes/com/goldenapp/MapsActivity.java
+++ app/src/main/java/twistedgiraffes/com/goldenapp/MapsActivity.java
@@ -2,15 +2,18 @@ package twistedgiraffes.com.goldenapp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final float DEFAULT_ZOOM = 13;
 
     private GoogleMap mMap;
 
@@ -28,8 +31,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -38,9 +40,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Set up map
+        // Add a marker in Golden, CO, USA and move the camera to Golden
+        LatLng golden = new LatLng(39.7554, -105.2213);
+        Marker goldenMarker = mMap.addMarker(new MarkerOptions().position(golden).title("Event in Golden, CO, USA"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(golden));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(DEFAULT_ZOOM));
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast toast = Toast.makeText(getBaseContext(), marker.getTitle(), Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+            }
+        });
     }
 }
