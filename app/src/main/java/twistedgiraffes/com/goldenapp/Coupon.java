16335c72f2975863b0a74facfc5d77b723f9a373
Michael Villafuerte <mvillafu@mymail.mines.edu>
2017-03-20 18:28:48 -0600

    Created a new class that will store the data from the db about the coupons.

---
+++ app/src/main/java/twistedgiraffes/com/goldenapp/Coupon.java
@@ -0,0 +1,35 @@
+package twistedgiraffes.com.goldenapp;
+
+/**
+ * Created by michael on 3/20/2017.
+ */
+
+public class Coupon {
+    private String mCoupon; // What the discount is
+    private double mLat;
+    private double mLog;
+
+    public String getmCoupon() {
+        return mCoupon;
+    }
+
+    public void setmCoupon(String mCoupon) {
+        this.mCoupon = mCoupon;
+    }
+
+    public double getmLat() {
+        return mLat;
+    }
+
+    public void setmLat(double mLat) {
+        this.mLat = mLat;
+    }
+
+    public double getmLog() {
+        return mLog;
+    }
+
+    public void setmLog(double mLog) {
+        this.mLog = mLog;
+    }
+}
