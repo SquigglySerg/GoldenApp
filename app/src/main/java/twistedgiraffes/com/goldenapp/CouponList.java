96732038522160c893a8302e939f72fd18763da3
Michael Villafuerte <mvillafu@mymail.mines.edu>
2017-03-22 10:09:03 -0600

    Created a new class called 'CouponList' to hold the coupons.

---
+++ app/src/main/java/twistedgiraffes/com/goldenapp/CouponList.java
@@ -0,0 +1,13 @@
+package twistedgiraffes.com.goldenapp;
+
+import java.util.List;
+
+/**
+ * Created by michael on 3/22/2017.
+ */
+
+public class CouponList {
+    private static CouponList sCouponList;
+
+    private List<Coupon> mCoupons;
+}
