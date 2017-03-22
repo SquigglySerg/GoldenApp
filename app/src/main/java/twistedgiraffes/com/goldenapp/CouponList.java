983239b4f4d725758ba80e6d8b5d60235a22a066
Michael Villafuerte <mvillafu@mymail.mines.edu>
2017-03-22 16:47:13 -0600

    Setting up the tests for the golden ticket.

--- app/src/main/java/twistedgiraffes/com/goldenapp/CouponList.java
+++ app/src/main/java/twistedgiraffes/com/goldenapp/CouponList.java
@@ -1,5 +1,8 @@
 package twistedgiraffes.com.goldenapp;
 
+import android.content.Context;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,5 +12,30 @@ import java.util.List;
 public class CouponList {
     private static CouponList sCouponList;
 
+    // This is my dummy set of strings
+    private String[] mString = {
+            "10% off your next meal at Woody's Wood-Fired Pizza",
+            "15% off your next purchase of tires from Big O Tires",
+            "You won $200 in cash",
+            "5% Your next one night stay at The Dove Inn Bed and Breakfast"
+    };
+
+    // Remove above when database is implemented
     private List<Coupon> mCoupons;
+
+    public static CouponList get(Context context) {
+        if (sCouponList == null) {
+            sCouponList = new CouponList(context);
+        }
+        return sCouponList;
+    }
+
+    private CouponList(Context context) {
+        mCoupons = new ArrayList<>();
+
+    }
+
+    public List<Coupon> getCoupons() {
+        return mCoupons;
+    }
 }
